ac314a3c90fdb49cb87aabdd158e0366cec6724c
Yu Jin Kim <yujinkim@fb.com>
2019-06-25 15:44:23 -0700

    version 1

---
+++ Tippy/SettingsVIewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  Tippy
+//
+//  Created by yujinkim on 6/25/19.
+//  Copyright © 2019 yujinkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -17,7 +21,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.title = @"Tip Calculator";
 }
 
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15), @(0.18), @(0.20)];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
 
 @end
